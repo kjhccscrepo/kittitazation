2170cee9d1543430b09097ccb6eb46649436fe31
kjhccscrepo <j_kist1@mail.harpercollege.edu>
2025-03-04 14:28:39 -0600

    Add files via upload

---
+++ Itty Bitty Kitty.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <string>
+#include <regex> //This is a library I found online, I could use string_name.find(), etc etc. But I liked how clean this was
+
+
+void catification (std::string &mutator) {
+	mutator = std::regex_replace(mutator, std::regex("cat"), "/ᐠ｡ꞈ｡ᐟ\\");
+}
+
+int main()
+{
+	std::string user_inputted_sentence;
+	std::cout << "Welcome to the Epic Sentence Converter!" << std::endl << std::endl << "Please enter a sentence: ";
+	std::getline (std::cin, user_inputted_sentence);
+	std::cout << std::endl << "Your sentence is:" << std::endl;
+	std::cout << "''" << user_inputted_sentence << "''" << std::endl;
+	catification(user_inputted_sentence);
+	std::cout << std::endl << "Your epicaified sentence is:" << std::endl;
+	std::cout << "''" << user_inputted_sentence << "''" << std::endl;
+	
+   
+}
